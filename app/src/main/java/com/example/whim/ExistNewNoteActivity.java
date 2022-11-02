1d4dd7285530a8dfa0f6022737d882f8fc48b035
Henry-Xing <664243890@qq.com>
2022-11-02 23:24:50 +1100

    Update ExistNewNoteActivity.java

--- app/src/main/java/com/example/whim/ExistNewNoteActivity.java
+++ app/src/main/java/com/example/whim/ExistNewNoteActivity.java
@@ -1,7 +1,5 @@
 package com.example.whim;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +10,6 @@ import androidx.core.content.FileProvider;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -21,7 +18,6 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.Html;
@@ -36,7 +32,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
-
 
 import com.example.whim.Models.Notes;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -57,7 +52,6 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -70,7 +64,7 @@ import java.util.Map;
 
 
 public class ExistNewNoteActivity extends AppCompatActivity {
-    private EditText inputNoteTitle, inputNoteText;
+    private EditText inputNoteTitle;
     private TextView textDateTime;
     private ImageView selectedImage;
     private String imageUri;
@@ -91,7 +85,6 @@ public class ExistNewNoteActivity extends AppCompatActivity {
     Button locationBtn;
     String currentPhotoPath;
 
-    TextView locationText;
     FusedLocationProviderClient fusedLocationProviderClient;
 
     public static final int CAMERA_PERM_CODE = 101;
@@ -105,6 +98,7 @@ public class ExistNewNoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note_acticity);
 
+//        initialise button and view
         textProgress = findViewById(R.id.textProgress);
         progressBar = findViewById(R.id.progress_loader);
 
@@ -113,36 +107,33 @@ public class ExistNewNoteActivity extends AppCompatActivity {
 
 
         inputNoteTitle = findViewById(R.id.storedTitle);
-        inputNoteText = findViewById(R.id.storedNote);
         textDateTime = findViewById(R.id.textDateTime1);
         selectedImage = findViewById(R.id.imageExist1);
-
-
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         cameraBtn = findViewById(R.id.camera11);
         galleryBtn = findViewById(R.id.gallery11);
         paletteBtn = findViewById(R.id.exist_palette);
-        locationBtn =findViewById(R.id.location2);
+        locationBtn = findViewById(R.id.location2);
 
-        SimpleDateFormat formatterTime = new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a");
+//        get instances from firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        Date notesDate = Calendar.getInstance().getTime();
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatterTime = new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a");
 
         storageReference = FirebaseStorage.getInstance().getReference();
 
+//        set date
         textDateTime.setText(
                 new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(new Date())
         );
 
-        TextView inputNoteText = (TextView)findViewById(R.id.storedNote);
+        TextView inputNoteText = (TextView) findViewById(R.id.storedNote);
         String img = getColoredSpanned("images", "#67B1F9");
-        String txt = getColoredSpanned("text","#FFCA3A");
-        String photos = getColoredSpanned("doodles","#6E80FA");
-        inputNoteText.setHint(Html.fromHtml("What is on your mind today? You can insert "+img+", "+txt+", or draw "+photos+"."));
+        String txt = getColoredSpanned("text", "#FFCA3A");
+        String photos = getColoredSpanned("doodles", "#6E80FA");
+        inputNoteText.setHint(Html.fromHtml("What is on your mind today? You can insert " + img + ", " + txt + ", or draw " + photos + "."));
 
 
         notes = new Notes();
@@ -189,13 +180,9 @@ public class ExistNewNoteActivity extends AppCompatActivity {
 
 
         // Note save button
-
         ImageView imageBackedit2 = findViewById(R.id.imageBack2);
         imageBackedit2.setOnClickListener(new View.OnClickListener() {
             @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(ExistNewNoteActivity.this, ExistUserMainPage.class));
-//            }
             public void onClick(View view) {
                 onBackPressed();
             }
@@ -226,8 +213,8 @@ public class ExistNewNoteActivity extends AppCompatActivity {
                     }
                     note.put("title", title);
                     note.put("content", content);
-                    note.put("image",imgUri);
-                    note.put("time",time);
+                    note.put("image", imgUri);
+                    note.put("time", time);
                     note.put("location", location);
                     note.put("searchkeyword", searchkeyword);
                     note.put("imagename", imgName);
@@ -269,9 +256,8 @@ public class ExistNewNoteActivity extends AppCompatActivity {
     private void askGalleryPermissions() {
         if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) !=
                 PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(ExistNewNoteActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, GALLERY_PERM_CODE);
-        }
-        else {
+            ActivityCompat.requestPermissions(ExistNewNoteActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, GALLERY_PERM_CODE);
+        } else {
             getGallery();
         }
     }
@@ -284,9 +270,8 @@ public class ExistNewNoteActivity extends AppCompatActivity {
     private void askLocationPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST_CODE);
-        }
-        else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else {
             getLocation();
         }
     }
@@ -311,8 +296,7 @@ public class ExistNewNoteActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }
-                else {
+                } else {
                     Toast.makeText(ExistNewNoteActivity.this, "Location null error", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -323,7 +307,7 @@ public class ExistNewNoteActivity extends AppCompatActivity {
     // Ask camera to take photo permission
     private void askCameraPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
         } else {
             dispatchTakePictureIntent();
         }
@@ -337,24 +321,21 @@ public class ExistNewNoteActivity extends AppCompatActivity {
         if (requestCode == GALLERY_PERM_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getGallery();
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Gallery Permission is Required to Use photo.", Toast.LENGTH_SHORT).show();
             }
         }
         if (requestCode == CAMERA_PERM_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 dispatchTakePictureIntent();
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
             }
         }
         if (requestCode == LOCATION_REQUEST_CODE) {
-            if (grantResults.length > 0 && (grantResults[0] + grantResults[1]== PackageManager.PERMISSION_GRANTED)) {
+            if (grantResults.length > 0 && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
                 getLocation();
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Location Permission is Required to Use location.", Toast.LENGTH_SHORT).show();
             }
         }
@@ -375,7 +356,7 @@ public class ExistNewNoteActivity extends AppCompatActivity {
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                 Uri contentUri = Uri.fromFile(f);
 
-                // 能不能吧image的uri存成string再之后转换
+                // transform image uri to string
                 imageUri = Uri.fromFile(f).toString();
                 imageName = f.getName();
                 mediaScanIntent.setData(contentUri);
@@ -393,8 +374,8 @@ public class ExistNewNoteActivity extends AppCompatActivity {
 
                 Uri contentUri = data.getData();
                 String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                String imageFileName = "JPEG_" + timeStamp +"."+getFileExt(contentUri);
-                Log.d("tag", "onActivityResult: Gallery Image Uri:  " +  imageFileName);
+                String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
+                Log.d("tag", "onActivityResult: Gallery Image Uri:  " + imageFileName);
                 //selectedImage.setImageURI(contentUri);
                 imageUri = contentUri.toString();
                 imageName = imageFileName;
@@ -405,7 +386,7 @@ public class ExistNewNoteActivity extends AppCompatActivity {
     }
 
 
-    private void uploadImageToFirebase(String name, Uri contentUri){
+    private void uploadImageToFirebase(String name, Uri contentUri) {
         StorageReference image = storageReference.child("photos/" + name);
         image.putFile(contentUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
@@ -430,18 +411,16 @@ public class ExistNewNoteActivity extends AppCompatActivity {
 
             }
         }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                double progress = (100 * snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
-                progressBar.setProgress((int)progress);
-                textProgress.setText(progress+" %");
+                double progress = (100 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                progressBar.setProgress((int) progress);
+                textProgress.setText(progress + " %");
             }
         });
 
-
     }
-
-    // we will see this in storage:  images/image.jpeg....
 
 
     private String getFileExt(Uri contentUri) {
@@ -453,7 +432,7 @@ public class ExistNewNoteActivity extends AppCompatActivity {
 
     private File createImageFile() throws IOException {
         // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
 //        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
@@ -469,7 +448,6 @@ public class ExistNewNoteActivity extends AppCompatActivity {
     }
 
 
-
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -478,7 +456,7 @@ public class ExistNewNoteActivity extends AppCompatActivity {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
-            } catch (IOException ex) {
+            } catch (IOException ignored) {
 
             }
             // Continue only if the File was successfully created
@@ -492,26 +470,11 @@ public class ExistNewNoteActivity extends AppCompatActivity {
         }
     }
 
-    private String getPathFromUri(Uri contentUri) {
-        String filePath;
-        Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
-        if (cursor == null) {
-            filePath = contentUri.getPath();
-        }
-        else {
-            cursor.moveToFirst();
-            int index = cursor.getColumnIndex("data");
-            filePath = cursor.getString(index);
-            cursor.close();
-        }
-        return filePath;
-    }
-
-    public ArrayList<String> generateKeyword(String title){
+    public ArrayList<String> generateKeyword(String title) {
         ArrayList<String> keywords = new ArrayList<String>();
-        for(int i = 0; i < title.length() - 1; i++){
-            for(int j = i+1; j <= title.length(); j++){
-                keywords.add(title.substring(i,j).toLowerCase(Locale.ROOT).trim());
+        for (int i = 0; i < title.length() - 1; i++) {
+            for (int j = i + 1; j <= title.length(); j++) {
+                keywords.add(title.substring(i, j).toLowerCase(Locale.ROOT).trim());
             }
         }
         return keywords;
