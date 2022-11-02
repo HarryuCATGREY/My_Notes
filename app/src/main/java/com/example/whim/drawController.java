ff3efd2017c2faade3797bd001ece77b3b7d6f73
Henry-Xing <664243890@qq.com>
2022-11-02 18:51:23 +1100

    add commence for draw activity

--- app/src/main/java/com/example/whim/drawController.java
+++ app/src/main/java/com/example/whim/drawController.java
@@ -1,15 +1,12 @@
 package com.example.whim;
 
-import android.content.Intent;
-import android.provider.ContactsContract;
+import java.util.UUID;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
-import java.util.UUID;
 import android.provider.MediaStore;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -18,11 +15,14 @@ import android.view.View.OnClickListener;
 import android.widget.Toast;
 
 public class drawController extends AppCompatActivity implements OnClickListener{
-
+//    painting space
     private DrawingView drawView;
-    private ImageButton currPaint, drawBtn, eraseBtn, newBtn, saveBtn;
+
+//    current brush
+    private ImageButton currPaint;
+
+//    brush size
     private float smallBrush, mediumBrush, largeBrush;
-    private ImageView backIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,49 +30,69 @@ public class drawController extends AppCompatActivity implements OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.draw);
 
+//      initialising button and view
+//        drawing background
         drawView = (DrawingView)findViewById(R.id.drawing);
+
+//        palette
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+
+//        change brush size button
+        ImageButton drawBtn = (ImageButton) findViewById(R.id.draw_btn);
+        drawBtn.setOnClickListener(this);
+
+//        change erase size button
+        ImageButton eraseBtn = (ImageButton) findViewById(R.id.erase_btn);
+        eraseBtn.setOnClickListener(this);
+
+//        create new canvas button
+        ImageButton newBtn = (ImageButton) findViewById(R.id.new_btn);
+        newBtn.setOnClickListener(this);
+
+//        save canvas button
+        ImageButton saveBtn = (ImageButton) findViewById(R.id.save_btn);
+        saveBtn.setOnClickListener(this);
+
+//        close canvas button
+        ImageView backIcon = findViewById(R.id.close);
+        backIcon.setOnClickListener((v) -> {onBackPressed();});
+
+//      set color of drawing background
         drawView.setColor("#FA5151");
 
-        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+//        current drawing initialising
         currPaint = (ImageButton)paintLayout.getChildAt(0);
 
+//        brush size defined
         smallBrush = getResources().getInteger(R.integer.small_size);
         mediumBrush = getResources().getInteger(R.integer.medium_size);
         largeBrush = getResources().getInteger(R.integer.large_size);
 
+//        brush initialised as medium size
         drawView.setBrushSize(mediumBrush);
 
-        drawBtn = (ImageButton)findViewById(R.id.draw_btn);
-        drawBtn.setOnClickListener(this);
 
-        eraseBtn = (ImageButton)findViewById(R.id.erase_btn);
-        eraseBtn.setOnClickListener(this);
-
-        newBtn = (ImageButton)findViewById(R.id.new_btn);
-        newBtn.setOnClickListener(this);
-
-        saveBtn = (ImageButton)findViewById(R.id.save_btn);
-        saveBtn.setOnClickListener(this);
-
-        backIcon = findViewById(R.id.close);
-        backIcon.setOnClickListener((v) -> {onBackPressed();});
     }
 
 
 
+//    function for color choose
     public void paintClicked(View view)
     {
+//        not erase
         drawView.setErase(false);
+//        set brush size
         drawView.setBrushSize(drawView.getLastBrushSize());
-        //use chosen color
+
+//        use chosen color
         if(view!=currPaint)
         {
-            //update color
+//            update color
             ImageButton imgView = (ImageButton)view;
             String color = view.getTag().toString();
             drawView.setColor(color);
 
-            //reflect in the UI
+//            reflect in the UI
             currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
             currPaint = (ImageButton)view;
         }
@@ -81,14 +101,18 @@ public class drawController extends AppCompatActivity implements OnClickListener
     @Override
     public void onClick(View view)
     {
-        //response click
+//        four click choice: new, draw, erase, save
+
         if(view.getId() == R.id.draw_btn)
         {
-            //draw button clicked
+//            if choosing draw brush
+//            draw button clicked
             final Dialog brushDialog = new Dialog(this);
             brushDialog.setTitle("Brush size:");
+//            pop out brush size chose
             brushDialog.setContentView(R.layout.brush_chooser);
 
+//            chose small brush size
             ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
             smallBtn.setOnClickListener(new OnClickListener() {
                 @Override
@@ -100,6 +124,7 @@ public class drawController extends AppCompatActivity implements OnClickListener
                 }
             });
 
+//            chose medium brush size
             ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
             mediumBtn.setOnClickListener(new OnClickListener() {
                 @Override
@@ -111,6 +136,7 @@ public class drawController extends AppCompatActivity implements OnClickListener
                 }
             });
 
+//            chose large brush size
             ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
             largeBtn.setOnClickListener(new OnClickListener() {
                 @Override
@@ -125,11 +151,14 @@ public class drawController extends AppCompatActivity implements OnClickListener
             brushDialog.show();
         }else if(view.getId() == R.id.erase_btn)
         {
-            //switch to erase - choose size
+//            if choosing eraser
+//            eraser button clicked
             final Dialog brushDialog = new Dialog(this);
+//            pop out eraser size chose
             brushDialog.setTitle("Eraser size:");
             brushDialog.setContentView(R.layout.brush_chooser);
 
+//            choose small size
             ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
             smallBtn.setOnClickListener(new OnClickListener() {
                 @Override
@@ -140,6 +169,7 @@ public class drawController extends AppCompatActivity implements OnClickListener
                 }
             });
 
+//            choose medium size
             ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
             mediumBtn.setOnClickListener(new OnClickListener() {
                 @Override
@@ -150,6 +180,7 @@ public class drawController extends AppCompatActivity implements OnClickListener
                 }
             });
 
+//            choose large size
             ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
             largeBtn.setOnClickListener(new OnClickListener() {
                 @Override
@@ -163,13 +194,15 @@ public class drawController extends AppCompatActivity implements OnClickListener
             brushDialog.show();
         }else if(view.getId() == R.id.new_btn)
         {
-            //new button
+//            if click new button
             AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
             newDialog.setTitle("new Drawing");
+//            confirm choose again
             newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
             newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+//                    clear all and start new canvas
                     drawView.startNew();
                     dialog.dismiss();
                 }
@@ -184,14 +217,15 @@ public class drawController extends AppCompatActivity implements OnClickListener
 
         }else if(view.getId() == R.id.save_btn)
         {
+//            if click save button
             AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
             saveDialog.setTitle("Save Drawing");
             saveDialog.setMessage("Save drawing to device Gallery?");
+//            confirm choose again
             saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    //save drawing
-                    //save drawing
+//                    save drawing
                     drawView.setDrawingCacheEnabled(true);
                     String imageSaved = MediaStore.Images.Media.insertImage(
                             getContentResolver(), drawView.getDrawingCache(),
